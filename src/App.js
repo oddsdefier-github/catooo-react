118863b7adceb8f14fdf71d405b8fc212b731286
Odds <panerjeffryjames@gmail.com>
2023-10-12 00:05:44 +0800

    resize the font

--- src/App.js
+++ src/App.js
@@ -71,9 +71,9 @@ function App() {
 function Header() {
   return (
     <header className="h-[10%] w-screen flex justify-between items-center px-10">
-      <h1 className="font-bold text-2xl text-gray-400">
+      <h1 className="font-bold text-3xl lg:text-5xl text-gray-400">
         catooo
-        <span className="text-lg font-extrabold text-indigo-600">.</span>
+        <span className="text-6xl font-extrabold text-indigo-600">.</span>
       </h1>
     </header>
   );
@@ -83,7 +83,7 @@ function Loading() {
   return (
     <section className="h-[70%] w-full grid place-items-center">
       <div role="status" className="loading w-72 animate-pulse">
-        {<svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+        {<svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
           <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
         </svg>}
       </div>
@@ -103,7 +103,7 @@ function CatImage({ url }) {
           className="absolute top-0 right-0 rounded-tr-lg p-2 rounded-bl-lg hover:bg-indigo-600 transition duration-200"
           title="Download Image"
         >
-          {<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="stroke-white w-5 h-5">
+          {<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="stroke-white w-5 h-5">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
           </svg>}
         </a>
@@ -142,7 +142,7 @@ function FetchButton({ showNextCatImage }) {
           }}
           className="overflow-clip rounded-full active:scale-110 transition-all duration-300 ease-in-out hover:bg-gray-100 focus:ring focus:ring-gray-200 bg-white shadow-lg p-2"
         >
-          {<img class="cat-icon w-9 h-9 rounded-full pointer-events-none" src={catIcon} alt="cat-icon" />}
+          {<img className="cat-icon w-9 h-9 rounded-full pointer-events-none" src={catIcon} alt="cat-icon" />}
         </button>
       </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-font-inter')({
+      a: -0.0223,
+      b: 0.185,
+      c: -0.1745,
+      baseFontSize: 16,
+      importFontFace: true,
+    })
+  ]
 }
 
